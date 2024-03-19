08c985c425de4995d653d9cc81c3871e81cec84a
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-03-19 13:40:57 +0100

    first time hearing of &apos;

--- app/components/TextSection.tsx
+++ app/components/TextSection.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 
-export const TextSection = (props: {}) => {
+export const TextSection = () => {
   return (
     <div className="w-[95vw] ml-2 md:ml-8 text-xl mb-[32px] mt-[16px] md:mt-[64px] lg:text-4xl">
       <p className="text-[#c3c3c3] font-mono">ABOUT:</p>
       <p className="text-[#f5f5f5] font-mono">
-        {" "}
-        Hi, my name is Stefan and I'm a Romanian developer based in Groningen,
-        The Netherlands.
+        Hi, my name is Stefan and I&apos;m a Romanian developer based in
+        Groningen, The Netherlands.
       </p>
       <p className="text-[#f5f5f5] font-mono">
-        {" "}
-        I'm focused on creating interactive digital experiences for the web and
-        mobile.
+        I&apos;m focused on creating interactive digital experiences for the web
+        and mobile.
       </p>
       <p className="text-[#f5f5f5] font-mono">
-        {" "}
         Currently I am a full time developer at Eldie, a cool little romanian
         startup.
       </p>
       <p className="text-[#f5f5f5] font-mono">
-        {" "}
         I like to brag about how using Neovim makes me code so much faster and I
         will try my hardest to make you change to Neovim or at least consider
         it.
